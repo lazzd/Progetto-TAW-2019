8f0ef1f80d2e0b055199a709880bfc48979e1d33
lazzd <lazzarodario@hotmail.it>
2019-07-30 02:11:07 +0200

    Part Cashier Orders

--- front/angular-front/src/app/components/User/Cashier/cashier-orders/cashier-orders.component.ts
+++ front/angular-front/src/app/components/User/Cashier/cashier-orders/cashier-orders.component.ts
@@ -1,5 +1,12 @@
 import { Component, OnInit } from '@angular/core';
 
+import { CashierOrdersService } from '../../../../services/User/Cashier/cashier-orders/cashier-orders.service';
+
+import { SocketService } from '../../../../services/socket/socket.service';
+
+import { Router } from "@angular/router";
+import { ResOrder } from 'src/app/classes/res_order';
+
 @Component({
   selector: 'app-cashier-orders',
   templateUrl: './cashier-orders.component.html',
@@ -7,9 +14,94 @@ import { Component, OnInit } from '@angular/core';
 })
 export class CashierOrdersComponent implements OnInit {
 
-  constructor() { }
+  view_orders: Boolean;
+  allNotCompleteOrder: ResOrder[];
+
+  constructor(
+    private cashierOrdersService: CashierOrdersService,
+    private router: Router,
+    private socketService: SocketService) { }
 
   ngOnInit() {
+    this.initIoConnection();
+    this.allNotCompleteOrder = [];
+    this.view_orders = false;
+    this.getAllNotCompleteOrder();
+  }
+
+  private initIoConnection(): void {
+    this.socketService.initSocket();
+
+    this.socketService
+      .arrivalSuborder()
+      .subscribe(Order => {
+        console.log("ARRIVAL", Order);
+        const indexPresent = this.allNotCompleteOrder.findIndex(elem => elem.id_order == Order.id_order);
+        if (indexPresent != -1)
+          this.allNotCompleteOrder[indexPresent] = new ResOrder(Order);
+        else {
+          console.log("IMPOSSIBLE");
+          this.allNotCompleteOrder.push(new ResOrder(Order));
+          if (this.allNotCompleteOrder.length > 0)
+            this.view_orders = true;
+          //this.view_tables = true;
+        }
+      });
+
+    this.socketService
+      .newSuborder()
+      .subscribe(Order => {
+        console.log("NEW", Order);
+        const indexPresent = this.allNotCompleteOrder.findIndex(elem => elem.id_order == Order.id_order);
+        if (indexPresent != -1)
+          this.allNotCompleteOrder[indexPresent] = new ResOrder(Order);
+        else {
+          console.log("sssssss");
+          this.allNotCompleteOrder.push(new ResOrder(Order));
+          if (this.allNotCompleteOrder.length > 0)
+            this.view_orders = true;
+          //this.view_tables = true;
+        }
+      })
+
+  }
+
+  async getAllNotCompleteOrder(): Promise<void> {
+    try {
+      let CashierBillServicePromise = await this.cashierOrdersService.getAllNotCompleteOrder();
+      // ritorna l'observable...
+      CashierBillServicePromise.subscribe(
+        (ResSub => {
+          // L'AccessToken è valido: o perchè NON era scaduto oppure perchè il refresh è avvenuto in maniara corretta
+          if (ResSub.length == 0) {
+            //this.view_tables = false;
+          }
+          else {
+            console.log(ResSub);
+            //for(let i=0;i<ResSub.length;++i)
+            //this.myTables.push(new Table(ResSub[i]));
+            ResSub.forEach(element => {
+              this.allNotCompleteOrder.push(new ResOrder(element));
+            });
+            if (this.allNotCompleteOrder.length > 0)
+              this.view_orders = true;
+            //this.view_tables = true;
+          }
+        }),
+        (ErrSub => {
+          // necessario il catch della promise non gestisce l'errore dell'observable
+          // E' avvenuto un errore con il refresh dell'AccessToken: è necessario un nuovo login
+          this.router.navigate(['/auth/login']);
+          // da andare in pagina di login
+          console.log("SEND ORDER err", ErrSub);
+        })
+      )
+    } catch (errorPromise) {
+      this.router.navigate(['/auth/login']);
+      // da andare in pagina di login, MA: sarebbe poi da fare un back a questa pagina quando si è fatto effettivamente il login
+      console.log("sono qui");
+      console.log("SEND ORDER err", errorPromise);
+    }
   }
 
 }
--- front/angular-front/src/app/services/User/Cashier/cashier-orders/cashier-orders.service.ts
+++ front/angular-front/src/app/services/User/Cashier/cashier-orders/cashier-orders.service.ts
@@ -1,9 +1,33 @@
 import { Injectable } from '@angular/core';
 
+import { Observable, of, throwError } from 'rxjs';
+
+import { HttpClient, HttpHeaders, HttpEventType, HttpResponse } from '@angular/common/http';
+
+// import for refresh-token
+import { RefreshTokenService } from '../../../refresh-token/refresh-token.service';
+import { ResOrder } from 'src/app/classes/res_order';
+
+const urlOrder = "http://localhost:3000/orders";
+
 @Injectable({
   providedIn: 'root'
 })
 export class CashierOrdersService {
 
-  constructor() { }
+  constructor(
+    private refreshToken: RefreshTokenService,
+    private http: HttpClient) { }
+
+    async getAllNotCompleteOrder(): Promise<Observable<Array<ResOrder>>> {
+      try {
+        let promRefeshToken = await this.refreshToken.refreshToken();
+        console.log(promRefeshToken);
+        // Ora posso fare la richiesta...
+        return this.http.get<Array<ResOrder>>(urlOrder);
+      } catch (ErrorRefreshToken) {
+        return throwError(ErrorRefreshToken);
+      }
+    }
+
 }
