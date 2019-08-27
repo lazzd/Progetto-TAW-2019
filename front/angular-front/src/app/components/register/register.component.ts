6bf84598cb3ad95155c419e6c65e169ff4dd691a
FrancescoVillani <francesco.villani99@gmail.com>
2019-08-27 18:41:02 +0200

    registration changes

--- front/angular-front/src/app/components/register/register.component.ts
+++ front/angular-front/src/app/components/register/register.component.ts
@@ -30,6 +30,14 @@ export class RegisterComponent implements OnInit {
   ];
 
   form_reg: FormGroup;
+  erroreLungPass: boolean;
+  erroreLungMail: boolean;
+  erroreMail: boolean;
+  erroreNomeEsistente: boolean;
+  regCompletata: boolean;
+  erroreVuoto: boolean;
+  erroreMailEsistente: boolean;
+  erroreLungNome: boolean;
 
   constructor(
     private registerService: RegisterService,
@@ -47,35 +55,57 @@ export class RegisterComponent implements OnInit {
 
   sendRegister(): void {
 
+    this.erroreNomeEsistente = false;
+    this.erroreMailEsistente = false;
+    this.erroreVuoto = false;
+    this.erroreLungPass = false;
+    this.erroreMail = false;
+    this.regCompletata = false;
+    this.erroreLungMail = false;
+    this.erroreLungNome = false;
+
     const name = this.form_reg.value.nameRegister;
     const email = this.form_reg.value.emailRegister;
     const password = this.form_reg.value.passwordRegister;
     const task = this.form_reg.value.taskRegister;
 
-    
+
     if (name && email && password && task) {
       this.registerService.sendRegister({ name, email, password, task } as Register)
         .subscribe(
           (res: any) => {
-            const name = this.jwt.decodeToken(res.AccessToken).name;
-            const task = this.jwt.decodeToken(res.AccessToken).task;
-            // anche con la registrazione devo mandare il token...
-            localStorage.setItem('AccessToken', res.AccessToken);
-            localStorage.setItem('RefreshToken', res.RefreshToken);
-            localStorage.setItem('UserName', name);
-            localStorage.setItem('UserTask', task);
-            console.log('ACCESS: ', res.AccessToken);
-            console.log('REFRESH: ', res.RefreshToken);
-            this.router.navigate(['/user']);
+
+            this.form_reg.reset();
+            this.regCompletata = true;
+
           },
           (err: any) => {
+
+            if (err.error === 'Name already exists') {
+              this.erroreNomeEsistente = true;
+            }
+            else if (err.error === 'Email already exists') {
+              this.erroreMailEsistente = true;
+            }
+            else if (err.error === '"name" length must be at least 6 characters long') {
+              this.erroreLungNome = true;
+            }
+            else if (err.error === '"email" length must be at least 6 characters long') {
+              this.erroreLungMail = true;
+            }
+            else if (err.error === '"email" must be a valid email') {
+              this.erroreMail = true;
+            }
+            else if (err.error === '"password" length must be at least 6 characters long') {
+              this.erroreLungPass = true;
+            }
             // text con errori DA SERVER
             console.log(err.error);
           });
     }
     else {
       // text con errori con cambi non presenti
-      console.log(name, email, password, task);
+      this.erroreVuoto = true;
     }
   }
 }
