a7beb5ff72f11afae592a6fe92b274d12ab5c8c9
Lazzaro <lazzarodario@hotmail.it>
2019-08-28 01:54:10 +0200

    Auto refresh emit Pie Chart

--- front/angular-front/src/app/components/User/Cashier/cashier-statistics/cashier-statistics.component.ts
+++ front/angular-front/src/app/components/User/Cashier/cashier-statistics/cashier-statistics.component.ts
@@ -70,10 +70,36 @@ export class CashierStatisticsComponent implements OnInit {
       .subscribe(user => {
         console.log("A NEW USER ACTION", user);
         const indexPresent = this.allUsers.findIndex(elem => elem.email == user.email);
-        if (indexPresent != -1)
+        // User già pushato e presente nel pie chart, ricerca per nomi univoci (i nomi dovrebbero essere univoci per costruzione db), oppure usa mail nel pie chart
+        if (indexPresent != -1) {
           this.allUsers[indexPresent] = new User(user);
+          if (user.task === 'waiter') {
+            const indexPie = this.pieChartLabelsWaiter.findIndex(elem => elem == user.name);
+            if (indexPie != -1)
+              this.pieChartDataWaiter[indexPie] = user.actions;
+          }
+          else if (user.task === 'barman') {
+            const indexPie = this.pieChartLabelsBarman.findIndex(elem => elem == user.name);
+            if (indexPie != -1)
+              this.pieChartDataBarman[indexPie] = user.actions;
+          }
+          else if (user.task === 'cook') {
+            const indexPie = this.pieChartLabelsChef.findIndex(elem => elem == user.name);
+            if (indexPie != -1)
+              this.pieChartDataChef[indexPie] = user.actions;
+          }
+          else if (user.task === 'cashier') {
+            const indexPie = this.pieChartLabelsCashier.findIndex(elem => elem == user.name);
+            if (indexPie != -1)
+              this.pieChartDataCashier[indexPie] = user.actions;
+          }
+        }
         else {
           this.allUsers.push(new User(user));
+          if (user.task === 'waiter') { this.pieChartLabelsWaiter.push(user.name); this.pieChartDataWaiter.push(user.actions); }
+          else if (user.task === 'barman') { this.pieChartLabelsBarman.push(user.name); this.pieChartDataBarman.push(user.actions); }
+          else if (user.task === 'cook') { this.pieChartLabelsChef.push(user.name); this.pieChartDataChef.push(user.actions); }
+          else if (user.task === 'cashier') { this.pieChartLabelsCashier.push(user.name); this.pieChartDataCashier.push(user.actions); }
           if (this.allUsers.length > 0)
             this.view_users = true;
           //this.view_tables = true;
@@ -103,12 +129,12 @@ export class CashierStatisticsComponent implements OnInit {
 
 
               for (let us of this.allUsers) {
-                if (us.task === 'waiter') { this.pieChartLabelsWaiter.push(us.name); this.pieChartDataWaiter.push(us.actions); } 
+                if (us.task === 'waiter') { this.pieChartLabelsWaiter.push(us.name); this.pieChartDataWaiter.push(us.actions); }
                 else if (us.task === 'barman') { this.pieChartLabelsBarman.push(us.name); this.pieChartDataBarman.push(us.actions); }
                 else if (us.task === 'cook') { this.pieChartLabelsChef.push(us.name); this.pieChartDataChef.push(us.actions); }
                 else if (us.task === 'cashier') { this.pieChartLabelsCashier.push(us.name); this.pieChartDataCashier.push(us.actions); }
               }
-              
+
 
 
 
