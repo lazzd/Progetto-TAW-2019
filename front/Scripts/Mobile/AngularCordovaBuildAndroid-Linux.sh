380ad7b55c28f6dfcf5dd73ea90fe9989c2ac55d
lazzd <lazzarodario@hotmail.it>
2019-10-17 18:54:28 +0200

    Re Scripts

--- front/Scripts/Mobile/AngularCordovaBuildAndroid-Linux.sh
+++ front/Scripts/Mobile/AngularCordovaBuildAndroid-Linux.sh
@@ -4,3 +4,5 @@ rm -f ../../angular-front/src/index.html
 rm -f ../../angular-front/src/main.ts
 cp -f ../../angular-front/MobileWeb/indexesMobileWeb/indexMobile.html ../../angular-front/src/index.html
 cp -f ../../angular-front/MobileWeb/mainsMobileWeb/mainMobile.ts ../../angular-front/src/main.ts
+cd ../../angular-front
+npm run mobile:build && cd logicaldishes && cordova build android
